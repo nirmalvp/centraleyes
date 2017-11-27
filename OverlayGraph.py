a94f3cc1a23770932d015a8a2164a1639dc3ab4f
Nirmal <nirmalvp1502@gmail.com>
2017-11-27 13:26:44 +0100

    First working version of heirarchical cover

--- OverlayGraph.py
+++ OverlayGraph.py
@@ -11,13 +11,45 @@ class OverlayGraph():
         except IOError:
             self._createOverLayGraph()
 
+    def _getAllNeighbourDetails(self, vertex, i) :
+        for (_, neighbourVertex, edgeLabel) in self._graphLayer[i-1].edges_incident(vertex):
+            if neighbourVertex != vertex : #A loop
+                intermediateVertices = edgeLabel.get("intermediateVertices", [])
+                yield (neighbourVertex, intermediateVertices)
+
+    def _createAllPathLayer(self, i) :
+        overlayLayer = DiGraph(multiedges=True)
+        print "Cover vertices = " , self._coveredVertices[i]
+        for vertex in self._coveredVertices[i]:
+            for (neighbourVertex, intermediateVerticesBetweenSourceAndNeighbour) in self._getAllNeighbourDetails(vertex, i):
+                if neighbourVertex in self._coveredVertices[i]:
+                    print "In cover" , vertex, intermediateVerticesBetweenSourceAndNeighbour, neighbourVertex
+                    overlayLayer.add_edge(vertex, neighbourVertex, {"intermediateVertices":intermediateVerticesBetweenSourceAndNeighbour})
+                ##If neighbour isnt in the cover, the neighbour of neightbour definitely is
+                else :
+                    for (neighbourOfNeighbourVertex, intermediateVerticesBetweenNeighbours) in self._getAllNeighbourDetails(neighbourVertex, i) :
+                        newIntermediateVertices = []
+                        if neighbourOfNeighbourVertex != vertex:
+                            newIntermediateVertices.extend(intermediateVerticesBetweenSourceAndNeighbour)
+                            newIntermediateVertices.append(neighbourVertex)
+                            newIntermediateVertices.extend(intermediateVerticesBetweenNeighbours)
+                            print "not cover" , vertex, newIntermediateVertices, neighbourOfNeighbourVertex
+                            overlayLayer.add_edge(vertex, neighbourOfNeighbourVertex, {"intermediateVertices":newIntermediateVertices})
+        return overlayLayer
+
+    def _createLayer(self, i):
+        undirectedGraph = Graph(self._graphLayer[i-1])
+        self._coveredVertices[i] = undirectedGraph.vertex_cover()
+        if cfg.ALLPATH:
+            self._graphLayer[i] = self._createAllPathLayer(i)
+
     def _createOverLayGraph(self):
         try :
             baseGraphFile = cfg.filelocation["BASEGRAPH"]
             numberOfLayers = cfg.numberOfLayers
             self._graphLayer = [[] for k in xrange(numberOfLayers)]
             self._coveredVertices = [[] for k in xrange(numberOfLayers)]
-            self._graphLayer[0] = pickle.load(open( baseGraphFile, "rb" ))
+            self._graphLayer[0] = DiGraph(pickle.load(open( baseGraphFile, "rb" )))
             self._coveredVertices[0] = self._graphLayer[0].vertices()
             for i in xrange(1, numberOfLayers) :
                 self._createLayer(i)
@@ -25,22 +57,5 @@ class OverlayGraph():
             print "No base file located"
             import sys
             sys.exit()
-
-    def _createlayer(self, i):
-        undirectedGraph = Graph(self._graphLayer[i-1])
-        self._coveredVertices[i] = undirectedGraph.vertex_cover()
-        if cfg.ALLPATH:
-            self._createAllPathLayer()
-
-    def _createAllPathLayer(self) :
-        overlayLayer = DiGraph(multiedges=True)
-
-
-
-
-
-
-
-
 
 overlay = OverlayGraph()
--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-filelocation = {"OVERLAYGRAPH":os.path.dirname(os.path.realpath(__file__)) + 'overlay.pickle',
-                "BASEGRAPH" : os.path.dirname(os.path.realpath(__file__)) + 'basegraph.pickle'}
+filelocation = {"OVERLAYGRAPH":os.path.dirname(os.path.realpath(__file__)) + '/overlay.pickle',
+                "BASEGRAPH" : os.path.dirname(os.path.realpath(__file__)) + '/basegraph.pickle'}
 numberOfLayers = 3
 ALLPATH = True
