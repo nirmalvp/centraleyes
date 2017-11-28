59ccf959e9ef849525019c8b366bd11c23f99b92
Nirmal <nirmalvp1502@gmail.com>
2017-11-28 11:35:21 +0100

    Implementing manual version of vertex cover

--- OverlayGraph.py
+++ OverlayGraph.py
@@ -26,6 +26,12 @@ class OverlayGraph():
             self._createOverLayGraph()
             self._writeOverLayGraphToFile()
 
+    def _plotLayer(self, layerNum, saveLocation) :
+        if layerNum >= cfg.numberOfLayers or layer < 0:
+            applogger.error("Layer doesnt exist")
+        else :
+            save(self._graphLayer[layerNum], saveLocation,axes=False,aspect_ratio=True)
+
     def _writeOverLayGraphToFile(self) :
         graphLayerList = []
         #WOrkaround since Digraph object can't be directly pickled. However, pickle works on networkx objects
@@ -38,8 +44,8 @@ class OverlayGraph():
             applogger.error("Error while writing overlayGraphFile")
 
     #Helper function to get the outgoing edges(including parallel edges) of a vertex and extract path information from them.
-    def _getAllNeighbourDetails(self, vertex, i) :
-        for (_, neighbourVertex, edgeLabel) in self._graphLayer[i-1].edges_incident(vertex):
+    def _getAllNeighbourDetails(self, vertex, layerNum) :
+        for (_, neighbourVertex, edgeLabel) in self._graphLayer[layerNum].edges_incident(vertex):
             if neighbourVertex != vertex : #If there is a loop, ignore it
             #Overlay Graph at layer i is a condensed graph of layer i-1.
             #Graph (A->C) at layer i have been could have been created out of graph(A->B->C) in layer(i-1)
@@ -50,19 +56,19 @@ class OverlayGraph():
 
     #Given a vertex v (included in the cover), this function creates an edge for every path from v to any other vertex that is also included
     #in the cover.
-    def createEdges(self, vertex, i) :
-        applogger.debug("considering vertex : %s,%s"%(vertex))
-        for (neighbourVertex, intermediateVerticesBetweenSourceAndNeighbour) in self._getAllNeighbourDetails(vertex, i):
+    def createEdges(self, vertex, layerNum) :
+        #applogger.debug("considering vertex : %s,%s"%(vertex))
+        for (neighbourVertex, intermediateVerticesBetweenSourceAndNeighbour) in self._getAllNeighbourDetails(vertex, layerNum - 1):
             #Graph (A->B->C). Imagine both A as well as B are included in the vertex cover(not optimum)
             #In the overlay graph, we add a edge from A to B and retain the intermediateVertices between A->B as it is in the lower layer
-            if neighbourVertex in self._coveredVertices[i]:
+            if neighbourVertex in self._coveredVertices[layerNum]:
                 self.overlayLayer.add_edge(vertex, neighbourVertex, {"intermediateVertices":intermediateVerticesBetweenSourceAndNeighbour})
             else :
                 ##If immediate neighbour isnt in the vertex cover, the neighbour of neighbour is definitely in the vertex cover
                 #or else the vertex cover property is violated.
                 #Imagine Graph (A-B-C), A and C are in the vertex cover, so we add an edge from A to C in the overlay Graph
                 #However the new edge's intermediateVertices are (intermediateVertices from A to B + Node B + intermediateVertices from B to C)
-                for (neighbourOfNeighbourVertex, intermediateVerticesBetweenNeighbours) in self._getAllNeighbourDetails(neighbourVertex, i) :
+                for (neighbourOfNeighbourVertex, intermediateVerticesBetweenNeighbours) in self._getAllNeighbourDetails(neighbourVertex, layerNum - 1) :
                     newIntermediateVertices = []
                     #Graph (A-B-A) could created an infinite loop. So we check if the neighbour of neighbour isnt the source vertex itself
                     if neighbourOfNeighbourVertex != vertex:
@@ -71,45 +77,44 @@ class OverlayGraph():
                         newIntermediateVertices.extend(intermediateVerticesBetweenNeighbours)
                         self.overlayLayer.add_edge(vertex, neighbourOfNeighbourVertex, {"intermediateVertices":newIntermediateVertices})
 
-    #This function retrieves all the edges between two nodes in an overlay graph. THere will be one edge per every route from a vertex to another
-    def _createAllPathLayer(self, i) :
+    #This function creates an overlay graph by considering every route from one covered vertex to another covered vertex in the base graph
+    def _createAllPathLayer(self, layerNum) :
         self.overlayLayer = DiGraph(multiedges=True)
-        #applogger.debug("Cover vertices = %s" %self._coveredVertices[i])
         pool = None
         #Edge creation between nodes on the overlay graph can be parallelized. Hence using a threadpool.
         #https://stackoverflow.com/questions/2846653/how-to-use-threading-in-python
         pool = ThreadPool()
-        pool.map(lambda v : self.createEdges(v,i), self._coveredVertices[i])
+        pool.map(lambda v : self.createEdges(v,layerNum), self._coveredVertices[layerNum])
         pool.close()
         pool.join()
         #At this point overlayLayer at level i is complete. That is all routes between vertex covers of layer (i-1) is connected via an edge
         return self.overlayLayer
 
-    #This function Creates the ith layer of the graph using the (i-1)th layer
-    def _createLayer(self, i):
-        #Find the vertex cover of the graph at the lower layer. Vertex cover works only on for undirected graph in Sage
-        #So, converting the lower layer into a undirected graph and also removing the multiple edges in the lower graph
-        undirectedGraph = Graph(self._graphLayer[i-1])
+    def _getVertexCoverOflayer(self, layerNum):
+        undirectedGraph = Graph(self._graphLayer[layerNum])
         undirectedGraph.remove_multiple_edges()
         undirectedGraph.allow_multiple_edges(False)
-        ##Remove
-        applogger.debug("Has multiple edge = %s" %undirectedGraph.has_multiple_edges())
-        applogger.debug("Real has multiple edge = %s" %self._graphLayer[i-1].has_multiple_edges())
-        applogger.debug("Number of vertices = %s"%len(undirectedGraph))
-        applogger.debug("Number of edges = %s"%undirectedGraph.size())
-        applogger.debug("NRandom edges = %s, %s, %s"%undirectedGraph.random_edge())
-        ##EndOfRemove
-        #g = undirectedGraph.plot()
-        #save(g,'/tmp/dom%s.png'%(i-1),axes=False,aspect_ratio=True)
-        #os.system('display /tmp/dom%s.png'%(i-1))
-        applogger.debug("Starting vertex cover of layer %s"%(i-1))
-        #coveredVertices ie the vertices of ith layer are the vertex cover of the (i-1)th layer
-        self._coveredVertices[i] = undirectedGraph.vertex_cover()
-        #self._coveredVertices[i] = undirectedGraph.vertex_cover(reduction_rules=False, algorithm='MILP')
-        applogger.debug("Retrieved vertex cover of layer %s"%(i-1))
+        vertexAndDegreeTupleList = [vertexAndDegreeTuple for vertexAndDegreeTuple in undirectedGraph.degree_iterator(labels=True)]
+        #Sort considering the order of the second key in the tuple ie degree, So that we have
+        #this list sorted by ascending order of degree
+        vertexAndDegreeTupleList.sort(key = lambda tup:tup[1])
+        vertexCover = set()
+        for vertex,_ in vertexAndDegreeTupleList:
+            if vertex not in vertexCover:
+                vertexCover.update(undirectedGraph.neighbors(vertex))
+        return vertexCover
+
+    #This function Creates the ith layer of the graph using the (i-1)th layer
+    def _createLayer(self, layerNum):
+        #Find the vertex cover of the graph at the lower layer. Vertex cover works only on for undirected graph in Sage
+        #So, converting the lower layer into a undirected graph and also removing the multiple edges in the lower graph
+        applogger.debug("Starting vertex cover of layer %s"%(layerNum-1))
+        self._coveredVertices[layerNum] = self._getVertexCoverOflayer(layerNum - 1)
+        applogger.debug("Retrieved vertex cover of layer %s"%(layerNum-1))
+        applogger.debug("VC Size = %s"%len(self._coveredVertices[layerNum]))
         #This is a switch in-case I decide to store only the shortest path later instead of all routes from one node to another
         if cfg.ALLPATH:
-            self._graphLayer[i] = self._createAllPathLayer(i)
+            self._graphLayer[layerNum] = self._createAllPathLayer(layerNum)
 
     def _createOverLayGraph(self):
         try :
@@ -125,15 +130,12 @@ class OverlayGraph():
             #Initialize the cover in the first layer as all the vertices of the raw graph
             self._coveredVertices[0] = self._graphLayer[0].vertices()
             applogger.debug("Retrieved baseGraph vertices")
-            for i in xrange(1, numberOfLayers) :
-                applogger.debug("Generating Layer %s"%i)
-                self._createLayer(i)
-                ##Remove
-                applogger.debug("allows_multiple_edges : %s"%self._graphLayer[i].allows_multiple_edges())
-                applogger.debug("has_multiple_edges : %s"%self._graphLayer[i].has_multiple_edges())
-                applogger.debug("has_loops : %s"%self._graphLayer[i].has_loops())
-                pickle.dump(self._graphLayer[i].networkx_graph(),open("/home/nirmal/%s.pickle"%i, "wb" ))
-                ##End of remove
+            for layerNum in xrange(1, numberOfLayers) :
+                applogger.debug("Generating Layer %s"%layerNum)
+                self._createLayer(layerNum)
+                applogger.debug("Layer %s Generated. Number of vertices = %s, Number of edges = %s"%(layerNum, self._graphLayer[layerNum].order(), self._graphLayer[layerNum].size()))
+                #pickle.dump(self._graphLayer[i].networkx_graph(),open("/home/nirmal/%s.pickle"%i, "wb" ))
+            #print self._graphLayer[numberOfLayers-1].edges(labels=True)
         except IOError :
             print "No base file located"
             import sys
--- logger.py
+++ logger.py
@@ -1,7 +1,7 @@
 import logging
 applogger = logging.getLogger('myapp')
 hdlr = logging.FileHandler('/var/tmp/myapp.log')
-formatter = logging.Formatter('%(thread)d %(threadName)s %(asctime)s %(levelname)s %(message)s')
+formatter = logging.Formatter('%(threadName)s %(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
 applogger.addHandler(hdlr)
 applogger.setLevel(logging.DEBUG)
