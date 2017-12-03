11ee368845a4d0ec19e701074cd0e9f7f2232eaa
Nirmal <nirmalvp1502@gmail.com>
2017-12-03 12:25:22 +0100

    Cleaning up sock.py

--- sock.py
+++ sock.py
@@ -1,8 +1,9 @@
 import socket
 import sys
 from thread import *
-#from sage.all import *
-#from OverlayGraph import OverlayGraph
+import json
+from sage.all import *
+from OverlayGraph import OverlayGraph
 
 HOST = ''   # Symbolic name meaning all available interfaces
 PORT = 12345 # Arbitrary non-privileged port
@@ -11,7 +12,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
 print socket.gethostbyname(socket.gethostname())
 
-#overlay = OverlayGraph()
+overlay = OverlayGraph()
 #Bind socket to local host and port
 try:
     s.bind(('', PORT))
@@ -28,24 +29,24 @@ print 'Socket now listening on PORT:', PORT
 #Function for handling connections. This will be used to create threads
 def clientthread(conn):
     #Sending message to connected client
-    conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
-
     #infinite loop so that function do not terminate and thread do not end.
     while True:
-
         #Receiving from client
-        data = conn.recv(1024)
-        #path = overlay.findOptimumRoute(overlay._graphLayer[0].random_vertex(), overlay._graphLayer[0].random_vertex(), {'dist' : 1})
-        #route = path[1]
-        #reply = str(route)
-        #print repr(data)
-        #print data
-        if data == '\r\n':
-            break
-        #conn.sendall(reply)
-        conn.sendall("HI")
-    #came out of loop
-    print "out of loop"
+        try :
+            data = conn.recv(1024)
+            data = data.strip()
+            if not data or data[0] != '{' :
+                break
+            data = json.loads(data)
+            args = data.get("args")
+            sourceVertex = tuple(args.get("sourceVertex"))
+            destVertex = tuple(args.get("destVertex"))
+            jsonDict = overlay.findOptimumRoute(sourceVertex, destVertex, {'dist' : 1})
+            response = json.dumps(jsonDict)
+        except:
+            print "error occured"
+            response = "Sorry, an error occured"
+        conn.sendall(response)
     conn.close()
 
 #now keep talking with the client
@@ -53,7 +54,6 @@ while 1:
     #wait to accept a connection - blocking call
     conn, addr = s.accept()
     print 'Connected with ' + addr[0] + ':' + str(addr[1])
-
     #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
     start_new_thread(clientthread ,(conn,))
 
